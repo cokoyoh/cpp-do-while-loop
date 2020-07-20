3134fbca2abda4b5a62d3166c6628d78d4d010d5
cokoyoh <charlesokoyoh@gmail.com>
2020-07-20 21:50:53 +0300

    Do while loop to output integers from 1 to 10

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int counter{1};
+
+    do {
+        cout << counter << " ";
+        counter++;
+    } while (counter <= 10);
+
+    cout << endl;
+}
